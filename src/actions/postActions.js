d6077c126cc924b2072ace1a8abb4a878505b450
ravitripathi <ravitripathi.biz@gmail.com>
2018-05-18 16:23:21 +0530

    createPost in Post Actions 57:29s

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,11 +1,26 @@
 import { FETCH_POSTS, NEW_POST } from './types';
 
 export  const fetchPosts = () => dispatch => {
-    console.log('fetching...');  
+    console.log('fetch posts...');  
     fetch('https://jsonplaceholder.typicode.com/posts') // fetch return a promise
             .then(res => res.json())
-            .then(posts => dispatch({
-                type: FETCH_POSTS,
-                payload: posts
-            }));
-}
+            .then(posts => 
+                dispatch({
+                    type: FETCH_POSTS,
+                    payload: posts
+                })
+            );
+};
+
+export  const createPost = (postData) => dispatch => {
+    console.log('create Post...');  
+    fetch('https://jsonplaceholder.typicode.com/posts',{
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(postData)
+    })
+    .then(res => res.json())
+    .then(data => console.log(data));
+};
--- src/components/Postform.js
+++ src/components/Postform.js
@@ -24,15 +24,7 @@ class Postform extends Component {
       body: this.state.body
     };
 
-    fetch('https://jsonplaceholder.typicode.com/posts',{
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify(post)
-    })
-    .then(res => res.json())
-    .then(data => console.log(data));
+    // call action
   }
   
   render() {
